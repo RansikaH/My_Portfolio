871022e02d607856f941fa45ff904b799d74e98a
RansikaH <ransikaheshani2000@gmail.com>
2024-07-04 13:56:28 +0530

    projects section

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -2,10 +2,16 @@ import styles from "./App.module.css";
 
 
 
+
+import { Projects } from "./components/Projects/Projects";
 function App() {
   return (
     <div className={styles.App}>
       
+
+  
+
+      <Projects />
     </div>
   );
 }
