26ef061b9176aae26643e7e8cd238f9444cd61ea
RansikaH <ransikaheshani2000@gmail.com>
2024-07-04 13:52:26 +0530

    about section

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,11 +1,13 @@
 import styles from "./App.module.css";
 
 
-
+import { About } from "./components/About/About";
 function App() {
   return (
     <div className={styles.App}>
       
+
+      <About />
     </div>
   );
 }
